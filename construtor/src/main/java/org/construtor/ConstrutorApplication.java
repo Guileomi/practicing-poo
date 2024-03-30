533e727722dee7d23bdcd7d1f5248e10e1eea107
Guileomi <guih.sousa123@gmail.com>
2024-03-29 23:10:44 -0300

    Subindo melhorias | encapsulamento

--- construtor/src/main/java/org/construtor/ConstrutorApplication.java
+++ construtor/src/main/java/org/construtor/ConstrutorApplication.java
@@ -23,6 +23,10 @@ public class ConstrutorApplication {
         //int quantity = sc.nextInt();
         Product product = new Product(name, price);
 
+        product.setName("Computer");
+        System.out.println("Updated name " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("updated price " + product.getPrice());
 
         System.out.println();
         System.out.println("Product data: " + product);
